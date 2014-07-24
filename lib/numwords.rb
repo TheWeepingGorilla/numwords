f4c8a454eed3699856902214872e34bc56590a6f
Sean Morrison <student@epicodus.com>
2014-07-24 09:53:40 -0700

    Added 20-90 test.  Used hash for key value pairs

--- lib/numwords.rb
+++ lib/numwords.rb
@@ -1,4 +1,9 @@
-def numwords (number)
-  one_to_twenty = ['zero','one']
+def one_to_twenty (number)
+  one_to_twenty = ['zero','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
   return one_to_twenty[number]
+end
+
+def twenty_to_ninety (number)
+  twenty_to_ninety = {20=>'twenty', 30=>'thirty',40=>'fourty',50=>'fifty',60=>'sixty',70=>'seventy',80=>'eighty',90=>'ninety'}
+  return twenty_to_ninety[number]
 end
--- spec/numwords_spec.rb
+++ spec/numwords_spec.rb
@@ -1,8 +1,20 @@
 require('rspec')
 require('numwords')
 
-describe('numwords') do
-  it('parses numerical input from 0-19 and returns word equivalent') do
-    numwords(1).should(eq('one'))
+describe('one_to_twenty') do
+  it('takes numerical input from 0-19 and returns word equivalent') do
+    one_to_twenty(1).should(eq('one'))
+  end
+  it('takes numerical input from 0-19 and returns word equivalent') do
+    one_to_twenty(11).should(eq('eleven'))
+  end
+  it('takes numerical input from 0-19 and returns word equivalent') do
+    one_to_twenty(19).should(eq('nineteen'))
+  end
+end
+
+describe('twenty_to_ninety') do
+  it('takes 20,30,...90 and returns word equivalent') do
+    twenty_to_ninety(20).should(eq('twenty'))
   end
 end
