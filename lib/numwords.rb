0e2a4b899a631a1c1c9bd1ee4461d83967a2d22e
Sean Morrison <student@epicodus.com>
2014-07-24 10:56:50 -0700

    one billion case working.  Some tests removed due to code refactor

--- lib/numwords.rb
+++ lib/numwords.rb
@@ -1,14 +1,44 @@
-def one_to_twenty (number)
-  one_to_twenty = ['zero','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
-  return one_to_twenty[number]
+def zero_to_twenty (number)
+  zero_to_twenty = ['zero','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
+  return zero_to_twenty[number]
 end
 
-def twenty_to_ninety (number)
-  twenty_to_ninety = {20=>'twenty', 30=>'thirty',40=>'fourty',50=>'fifty',60=>'sixty',70=>'seventy',80=>'eighty',90=>'ninety'}
-  return twenty_to_ninety[number]
+# def twenty_to_ninety (number)
+#   twenty_to_ninety = {20=>'twenty', 30=>'thirty',40=>'fourty',50=>'fifty',60=>'sixty',70=>'seventy',80=>'eighty',90=>'ninety'}
+#   return twenty_to_ninety[number]
+# end
+
+# def hundreds (number)
+#   hundreds = {100=>'one hundred',200=>'two hundred',300=>'three hundred',400=>'four hundred',500=>'five hundred',600=>'six hundred',700=>'seven hundred',800=>'eight hundred',900=>'nine hundred'}
+#   return hundreds[number]
+# end
+
+# def thousands (number)
+#   thousands = {1000=>'one thousand',2000=>'two thousand',3000=>'three thousand',4000=>'four thousand',5000=>'five thousand',6000=>'six thousand',7000=>'seven thousand',8000=>'eight thousand',9000=>'nine thousand'}
+#   return thousands[number]
+# end
+
+def ten_to_ninety (index)
+  ten_to_ninety = ['zero','ten','twenty','thirty','fourty','fifty','sixty','seventy','eighty','ninety']
+  return ten_to_ninety[index]
 end
 
-def hundreds (number)
-  hundreds = {100=>'one hundred',200=>'two hundred',300=>'three hundred',400=>'four hundred',500=>'five hundred',600=>'six hundred',700=>'seven hundred',800=>'eight hundred',900=>'nine hundred'}
-  return hundreds[number]
+def parse (num)
+  return_string = []
+
+  num_billions = num / 1000000000
+  num = num % 1000000000
+
+  if ( (num_billions > 0) && (num_billions < 21) )
+    return_string.push(zero_to_twenty(num_billions))
+  elsif ( (num_billions > 20) && (num_billions < 100) )
+    return_string.push(ten_to_ninety(num_billions / 10))
+    if (num_billions % 10 != 0)
+      return_string.push("-" + zero_to_twenty(num_billions % 10))
+    end
+  end
+  if (num_billions > 0)
+    return_string.push(" billion")
+  end
+return return_string.join
 end
--- spec/numwords_spec.rb
+++ spec/numwords_spec.rb
@@ -1,31 +1,50 @@
 require('rspec')
 require('numwords')
 
-describe('one_to_twenty') do
+describe('zero_to_twenty') do
   it('takes numerical input from 0-19 and returns word equivalent') do
-    one_to_twenty(1).should(eq('one'))
+    zero_to_twenty(1).should(eq('one'))
   end
   it('takes numerical input from 0-19 and returns word equivalent') do
-    one_to_twenty(11).should(eq('eleven'))
+    zero_to_twenty(11).should(eq('eleven'))
   end
   it('takes numerical input from 0-19 and returns word equivalent') do
-    one_to_twenty(19).should(eq('nineteen'))
+    zero_to_twenty(19).should(eq('nineteen'))
   end
 end
 
-describe('twenty_to_ninety') do
-  it('takes 20,30,...90 and returns word equivalent') do
-    twenty_to_ninety(20).should(eq('twenty'))
-  end
-  it('takes 20,30,...90 and returns word equivalent') do
-    twenty_to_ninety(90).should(eq('ninety'))
+# describe('twenty_to_ninety') do
+#   it('takes 20,30,...90 and returns word equivalent') do
+#     twenty_to_ninety(20).should(eq('twenty'))
+#   end
+#   it('takes 20,30,...90 and returns word equivalent') do
+#     twenty_to_ninety(90).should(eq('ninety'))
+#   end
+# end
+
+# describe('hundreds') do
+#   it('takes 100,200,...900 and returns word equivalent') do
+#     hundreds(100).should(eq('one hundred'))
+#   end
+#   it('takes 100,200,...900 and returns word equivalent') do
+#     hundreds(900).should(eq('nine hundred'))
+#   end
+# end
+
+# describe('thousands') do
+#   it('takes 1000,2000,...9000 and returns word equivalent') do
+#     thousands(1000).should(eq('one thousand'))
+#   end
+#   it('takes 1000,2000,...9000 and returns word equivalent') do
+#     thousands(9000).should(eq('nine thousand'))
+#   end
+# end
+
+describe('parse') do
+  it('takes 1 000 000 000... 9 000 000 000 and returns word equivalent') do
+    parse(1000000000).should(eq('one billion'))
   end
 end
-describe('hundreds') do
-  it('takes 100,200,...900 and returns word equivalent') do
-    hundreds(100).should(eq('one hundred'))
-  end
-  it('takes 100,200,...900 and returns word equivalent') do
-    hundreds(900).should(eq('nine hundred'))
-  end
-end
+
+
+
